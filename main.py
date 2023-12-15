29ec7a78721ddf7ab6a824a7b85c05a28704d116
Dinh <luongnguyendinhgreen@gmail.com>
2023-12-15 19:57:55 +0700

    Upload new data

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from dataprocess.process import DataProcess
 import requests
 
 data = DataProcess(dataframe_path="ikea_products_mentor_data.csv")
+i=0
 
 for (index,link) in enumerate(data.product_link):
-    response = requests.get(link)
-    if str(response.url) == "https://www.ikea.com/sa/en/cat/products-products/":
-        data.delete_colunm(index=index)
+    try:
+        response = requests.get(link)
+        if str(response.url) == "https://www.ikea.com/sa/en/cat/products-products/":
+            data.delete_colunm(index=index)
+            i+=1
+            print(f"{i} products have been deleted")
+    except:
+        print("Surpass")
 
-data.dataframe.to_csv()
+data.dataframe.to_csv("new_data.csv")
 
 
